4691235ef70f9a00c5d0519cb4f80ecf106e3113
Anna Kuś <aniaa.kus@gmail.com>
2023-05-13 10:28:32 +0200

    Add select preferences functionality

--- src/components/SelectBox.jsx
+++ src/components/SelectBox.jsx
@@ -1,8 +1,22 @@
-export default function SelectBox ({header, description}) {
+export default function SelectBox ({header, description, active, id, setSelectOptionsState}) {
+
+    function handleClick(e) {
+        const id = e.target.getAttribute("data-id")
+        setSelectOptionsState(() => {
+            const newState = {
+                1: false,
+                2: false,
+                3: false
+            }
+            newState[id] = true
+            return newState
+        })
+    }
+
     return (
-        <div className="mb-4 px-6 py-6 bg-lightGrey rounded-lg">
-            <h3 className="font-serif text-lg">{header}</h3>
-            <p>{description}</p>
+        <div onClick={(e) => handleClick(e)} data-id={id} className={`mb-4 px-6 py-6 rounded-lg ${active ? '' : 'hover:bg-paleOrange'} ${active ? 'bg-darkCyan' : 'bg-lightGrey'}`}>
+            <h3 data-id={id} className="font-serif text-lg">{header}</h3>
+            <p data-id={id}>{description}</p>
         </div>
     )
 }
--- src/components/SelectSection.jsx
+++ src/components/SelectSection.jsx
@@ -1,20 +1,40 @@
+import { useState } from "react"
 import SelectBox from "./SelectBox"
 
-export default function SelectSection ({name, active, header, options}) {
+export default function SelectSection ({name, active, header, options, setActiveSelection}) {
+
+    const [selectOptionsState, setSelectOptionsState] = useState({
+        1: false, 
+        2: false,
+        3: false
+    })
+
+
+    function handleActiveSelect() {
+        setActiveSelection(prev => {
+            return {
+                ...prev,
+                [name]: !prev[name]
+            }
+        })
+    }
+
+    const arrow = (<svg onClick={handleActiveSelect} width="19" height="13" xmlns="http://www.w3.org/2000/svg"><path d="M15.949.586l2.828 2.828-9.096 9.096L.586 3.414 3.414.586l6.267 6.267z" fill="#0E8784" fillRule="nonzero"/></svg>)
+    const reverseArrow = (<svg onClick={handleActiveSelect} width="19" height="13" className="block, rotate-180"  xmlns="http://www.w3.org/2000/svg"><path d="M15.949.586l2.828 2.828-9.096 9.096L.586 3.414 3.414.586l6.267 6.267z" fill="#0E8784" fillRule="nonzero"/></svg>)
 
     const content = options.map(box => {
         return (
-            <SelectBox key={box.name} header={box.header} description={box.description} />
+            <SelectBox key={box.header} header={box.header} description={box.description} id={box.id} setSelectOptionsState={setSelectOptionsState} active={selectOptionsState[box.id]} />
         )
     })
 
     return (
         <div className="mx-6 mb-24">
-            <div className="flex">
+            <div className="flex justify-between">
             <h2 className="mb-[18px] font-serif text-grey text-lg">{header}</h2>
-            {active ? <img src="../../public/images/plan/desktop/icon-arrow.svg" /> : <img />}
+            {active ? reverseArrow : arrow}
             </div>
-            {content}
+            {active ? content : ""}
         </div>
     )
 }
